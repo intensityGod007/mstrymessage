6d0be03a36cfeb41adcd23a2b4919c5578a39340
Hitesh Kumar <h.kumar96321@gmail.com>
2026-01-13 19:44:20 +0530

    initial commit

---
+++ src/app/(app)/layout.tsx
@@ -0,0 +1,28 @@
+
+import Navbar from "@/components/navbar";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "MstryMessage",
+  description: "Anonymous messages sharing web",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    
+    <>
+     
+       <Navbar/>
+        {children}
+  
+        </>
+    
+  );
+}
